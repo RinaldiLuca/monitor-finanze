b58cb52a08bc3023ebdffc68f5d0e3811b3b5f35
lucarinna <lucarinaldi.uni@gmail.com>
2025-07-26 13:50:47 +0200

    initializing data model

--- data_models/transaction.py
+++ data_models/transaction.py
@@ -1,1 +1,14 @@
 from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+
+class ApiTransactionRaw(BaseModel):
+    
+class Transaction(BaseModel):
+    id_transaction: str
+    transaction_dt: date
+    value_dt: date
+    amount: float
+    description: str
+    id_account: str
+    id_source: str
