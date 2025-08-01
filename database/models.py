6fa3fe1902859d268c274056b4dfb3e7c8621388
lucarinna <lucarinaldi.uni@gmail.com>
2025-08-01 19:13:15 +0200

    polishing ORM transaction

--- database/models.py
+++ database/models.py
@@ -1,18 +1,19 @@
-from sqlalchemy import Column, Integer, String, Float, Date
 from database.base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import date
 
 
 class Transaction(Base):
     __tablename__ = "transactions"
 
-    id = Column(Integer, primary_key=True, index=True)
-    external_id = Column(
-        String, unique=True, nullable=True, index=True
-    )  # id esterno opzionale
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    external_id: Mapped[str | None] = mapped_column(
+        unique=True, index=True, nullable=True
+    )
 
-    operation_date = Column(Date, nullable=False)
-    value_date = Column(Date, nullable=False)
-    amount = Column(Float, nullable=False)
-    description = Column(String, nullable=False)
-    category = Column(String, nullable=True)
-    account = Column(String, nullable=True)
+    operation_date: Mapped[date] = mapped_column(nullable=False)
+    value_date: Mapped[date] = mapped_column(nullable=False)
+    amount: Mapped[float] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    category: Mapped[str | None] = mapped_column(nullable=True)
+    account: Mapped[str | None] = mapped_column(nullable=True)
