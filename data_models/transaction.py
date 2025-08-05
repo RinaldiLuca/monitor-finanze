19d5fb14b483b075f222e28c1cd52f069ef4f2aa
lucarinna <lucarinaldi.uni@gmail.com>
2025-08-06 01:45:22 +0200

    refined version of ocr module (on ipynb), need to polish transaction

--- data_models/transaction.py
+++ data_models/transaction.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, field_validator
-from datetime import date
+from datetime import date, datetime
 from typing import Literal
 import hashlib
 
@@ -10,11 +10,41 @@ class BaseTransaction(BaseModel):
     amount: float
     description: str | None = None
 
-    @field_validator("description")
+    @field_validator("description", mode="before")
     @classmethod
-    def trim_description(cls, v):
-        if v is not None:
-            return v.strip()
+    def parse_string(cls, v):
+        if v.strip() == "":
+            return None
+        else:
+            return v
+
+    @staticmethod
+    def parse_float(v) -> float:
+        if isinstance(v, str):
+            if v == "":
+                return 0
+            elif "," in v:
+                return float(v.replace(".", "").replace(",", "."))
+            else:
+                raise ValueError
+        else:
+            return v
+
+    @field_validator("amount", mode="before")
+    @classmethod
+    def parse_amount(cls, v):
+        return cls.parse_float(v)
+
+    @field_validator("booking_dt", "value_dt", mode="before")
+    @classmethod
+    def parse_dates(cls, v):
+        if isinstance(v, str):
+            for fmt in ("%d/%m/%Y", "%Y-%m-%d"):
+                try:
+                    return datetime.strptime(v, fmt).date()
+                except ValueError:
+                    continue
+            raise ValueError(f"Data in formato non valido: {v}")
         return v
 
 
@@ -27,11 +57,19 @@ class OcrTransactionRaw(BaseTransaction):
     source_file_id: str | None = None  # filename+rownumber
     filename: str
     row: int
+    negative_amount: float | None = None
     source: Literal["pdf"] = "pdf"
+
+    @field_validator("negative_amount", mode="before")
+    @classmethod
+    def parse_negative_amount(cls, v):
+        return cls.parse_float(v)
 
     def model_post_init(self, __context):
         if self.filename and self.row is not None:
             self.source_file_id = f"{self.filename}__{self.row}"
+        if self.negative_amount is not None:
+            self.amount -= self.negative_amount
 
 
 class ConsolidatedTransaction(BaseTransaction):
