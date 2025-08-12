716fc50774629ed35cb987c23af5c8d3de012c8f
lucarinna <lucarinaldi.uni@gmail.com>
2025-08-13 01:15:31 +0200

    implementing a draft for the transactions table

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.ext.asyncio import AsyncSession
 from backend.dependancies import get_db
 from database.schemas import TransactionRead
@@ -8,6 +9,13 @@ from database.crud import load_existing_transactions
 DBSession = Annotated[AsyncSession, Depends(get_db)]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Origine del tuo frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/transactions", response_model=list[TransactionRead])
